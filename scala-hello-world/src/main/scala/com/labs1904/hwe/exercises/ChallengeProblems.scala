12f6fe7aba70b0bdd07b3c8c8bf1bf8cf7f3cc3c
Maeve Blanton <maeveblanton42@gmail.com>
2022-11-21 14:28:40 -0600

    Week 1 Lab

--- scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/ChallengeProblems.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/ChallengeProblems.scala
@@ -8,60 +8,79 @@ object ChallengeProblems {
     Returns - String
   */
 
+  def sameString(s: String): String = {
+    s
+  }
 
   /*
   2. Write a function that returns "Hello World!" and takes in nothing as a parameter
   Params - None
   Returns - String
    */
-
+  def helloWorld(): String = {
+    "Hello World!"
+  }
   /*
   3. Write a function that takes in a list and returns the total size of the list
   -Note - Use the .size method
   Params - List[Int]
   Returns - Int
    */
-
+  def listSize(list: List[Any]): Int = {
+    list.size
+  }
   /*
   4. Write a function that takes in an int and adds an int that you create within the function and returns the addition of the two together
   Note - Your variable must be a val and must be equal to 25
   Params - Int
   Returns - Int
    */
-
-
+  def sumInts(int: Int): Int = {
+    val add = 25
+    int + add
+  }
   /*
    5. Write a function that takes in a list of strings, and return a list of strings where every letter is capitalized
    Hint - you can use .map here
    Params - List[String]
    Returns - List[String]
-*/
-
+  */
+  def upper(strings: List[String]): List[String] = {
+    strings.map(_.toUpperCase)
+  }
   /*
   6. Write a function that returns a new list, where only elements of the list passed in that are 0 or positive numbers are kept.
   Params - List[Int]
   Returns - List[Int]
    */
-
+  def filterNegatives(ints: List[Int]): List[Int] = {
+    ints.filter(_ >= 0)
+  }
   /*
   7. Returns a new list, where only the elements passed in containing "car" are kept to the new list.
   Params - List[String]
   Returns - List[String]
- */
-
+  */
+  def containsCar(strings: List[String]): List[String] = {
+    strings.filter(_.contains("car"))
+  }
   /*
     8. Returns the sum of all numbers passed in.
     Params - List[Int]
     Returns - Int
    */
-
+  def sumList(ints: List[Int]): Int = {
+    ints.sum
+  }
   /*
   9. Write a function that takes in an integer with a cats age, and return the human age equivalent.
     A human year is equivalent to 4 cat years
     Params - Int
     Returns - Int
    */
-
+  def catsAge(age: Int): Int = {
+    age * 4
+  }
   /*
   10. Same question as #9, but this time you are given a Option[Int]
     If an int is provided, returns a cats age for the human's age equivalent.
@@ -69,27 +88,23 @@ object ChallengeProblems {
     A humanYear is equivalent to four catYears
     -Params - Option[Int]
     -Returns - Option[Int]
- */
-
+   */
+  def catsAgeOption(age: Option[Int]): Option[Int] = {
+    age.map(_ * 4)
+  }
   /*
   11. Write a function that takes in a list of ints, and return the minimum of the ints provided
   Params - List
   Returns - Int
-   */
-
+  */
+  def minimum(ints: List[Int]): Int = {
+    ints.min
+  }
   /*
   12. Same as question 11, but this time you are given a list of Option[Ints], returns the minimum of the Ints provided.
   If no ints are provided, return None.
- */
-
-
-
-
-
-
-
-
-
-
-
+  */
+  def minimumOption(ints: List[Option[Int]]): Option[Int] = {
+    ints.min
+  }
 }
